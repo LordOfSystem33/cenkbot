498f66919380897ce1ef95f847f88074927207d7
LordOfSystem33 <41236843+LordOfSystem33@users.noreply.github.com>
2018-10-10 20:09:09 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -8,7 +8,12 @@ exports.run = (client, message, params) => {
   .setTitle("Komutlar")
   .setDescription('')
   .setColor(0x00ffff)
-  .addField("**Bakımdayız!**)
+  .addField("**Eğlence ve Kullanıcı Komutları:**", `/banned = Dene ve Gör! \n/avatarım = Avatarınınızı Gösterir. \n/herkesebendençay = Herkese Çay Alırsınız. \n/koş = Koşarsınız.\n/çayiç = Çay İçersiniz. \n/çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \n/çayaşekerat = Çaya Şeker Atarsınız. \n/yumruh-at = Yumruk Atarsınız. \n/yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \n/sunucuresmi = BOT Sunucunun Resmini Atar. \n/sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \n/kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
+  .addField("**Sunucu Yetkilisi Komutları**", `/ban = İstediğiniz Kişiyi Sunucudan Banlar. \n/kick  = İstediğiniz Kişiyi Sunucudan Atar. \n/unban = İstediğiniz Kişinin Yasağını Açar. \n/sustur = İstediğiniz Kişiyi Susturur. -MUTED- \n/oylama = Oylama Açar. \n/duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
+  .addField("**Botun Ana Komutları**", "/yardım = BOT Komutlarını Atar. \n/bilgi = BOT Kendisi Hakkında Bilgi Verir. \n/ping = BOT Gecikme Süresini Söyler. \n/davet = BOT Davet Linkini Atar. \n/istatistik = BOT İstatistiklerini Atar.")
+  .addField("**Yapımcı:**", " **MassiveStroke#7934** ")
+  .addField("**Sürüm:**",  " **Beta v1.0 Çok Yakında** ")
+  .setFooter('___Bakımdayız!___')
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
